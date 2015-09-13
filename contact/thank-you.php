e4da741192ec8d833a24e7fec793ae146467604e
Liane Makatura <liane@dali.dartmouth.edu>
2015-09-13 10:46:38 -0400

    first round of source code; developed 9/10-9/13. Includes initial layout of everything but the blog and the reel.

---
+++ contact/thank-you.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"  "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en-US" lang="en-US">
+<head>
+    <meta http-equiv='Content-Type' content='text/html; charset=utf-8'/>
+
+      	<!-- bootstrap -->
+	<link href="bootstrap-3.3.4-dist/css/bootstrap.min.css" rel="stylesheet">
+	
+	<!-- carousel -->
+  	<link rel="stylesheet" href="css/gallery-css/gallery.theme.css">
+	<link rel="stylesheet" href="css/gallery-css/gallery.min.css">
+
+	<!-- custom css -->
+  	<link type="css/text" rel="stylesheet" href="css/global.css"/>
+
+  	<!-- custom fonts-->
+  	<link href="http://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css">
+
+    <title>Thank you!</title>
+    <link rel="STYLESHEET" type="text/css" href="contact.css">
+</head>
+<body>
+
+<h2>Thanks for contacting us!</h2>
+
+</body>
+</html>
